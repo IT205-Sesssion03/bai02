fa4c3f736597e308723e0f37e4c67d42445bb9b3
Vu Dinh Anh Tuan <vudinhanhtuan26@gmail.com>
2026-05-26 23:32:38 +0700

    M

--- bai02.py
+++ bai02.py
@@ -21,5 +21,5 @@ for employee_number in range(1, 4):
         print("CẢNH BÁO: Nhân viên nghỉ cả tháng. Không xét duyệt thưởng.")
         continue 
     bonus_amount = working_days * 200000
-    print(f" Đã tự động gửi Email: 'Chúc mừng nhận được {bonus_amount} VNĐ tiền thưởng!'")
+    print(f" Đã gửi Email: 'Chúc mừng nhận được {bonus_amount} VNĐ tiền thưởng!'")
 print("Đã hoàn tất quá trình duyệt thưởng cho 3 nhân viên!")
